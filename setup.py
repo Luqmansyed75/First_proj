340c2b90ba9bb533b26007a08cab97d229d1dd0c
Luqmansyed75 <luqmanlukku20@gmail.com>
2025-09-21 21:45:46 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+''' this fun will retrun the list of requirements'''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+setup(
+name='first_mlproject',
+version='0.0.1',
+author='luqman',
+author_email='luqmanlukku20@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
