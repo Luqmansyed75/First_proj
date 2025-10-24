833f425d2499b827f57d098c635f3e5c520cc68c
Luqmansyed75 <luqmanlukku20@gmail.com>
2025-10-25 00:09:49 +0530

    Deplayment and Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+import pickle
 
 def save_object(file_path,obj):
     try:
@@ -41,3 +42,10 @@ def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     except Exception as e:
         raise CustomException(e, sys)
 
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
